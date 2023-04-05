53a830b6d81beb7626153dd57ce911f7755eac51
Daniel Larkin <realdanieljlarkin@gmail.com>
2023-04-04 20:24:50 -0500

    Fixed fixed type error

--- src/server/helpers/filterUser.ts
+++ src/server/helpers/filterUser.ts
@@ -1,7 +1,8 @@
 import type { User } from '@clerk/nextjs/dist/api'
 
 export const filterUserData = (user: User) => {
-    const fullName = user.firstName! + ' ' + user.lastName
+    // const fullName = user.firstName! + ' ' + user.lastName
+    const fullName = `${user.firstName} ${user.lastName}`
     const username = user.username
     return {
         id: user.id,
