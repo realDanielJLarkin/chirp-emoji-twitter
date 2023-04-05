e29b817b133a0f94c36f0798a0afd372f4bdd88b
Daniel Larkin <realdanieljlarkin@gmail.com>
2023-04-04 20:27:08 -0500

    Still working on that type error

--- src/server/helpers/filterUser.ts
+++ src/server/helpers/filterUser.ts
@@ -2,7 +2,7 @@ import type { User } from '@clerk/nextjs/dist/api'
 
 export const filterUserData = (user: User) => {
     // const fullName = user.firstName! + ' ' + user.lastName
-    const fullName = `${user.firstName} ${user.lastName}`
+    const fullName: string | null = `${user.firstName} ${user.lastName}`
     const username = user.username
     return {
         id: user.id,
