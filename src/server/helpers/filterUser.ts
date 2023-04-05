dadca80a1aebfd7544884335a300a32aef8d09b4
Daniel Larkin <realdanieljlarkin@gmail.com>
2023-04-04 20:31:47 -0500

    Type error

--- src/server/helpers/filterUser.ts
+++ src/server/helpers/filterUser.ts
@@ -2,12 +2,13 @@ import type { User } from '@clerk/nextjs/dist/api'
 
 export const filterUserData = (user: User) => {
     // const fullName = user.firstName! + ' ' + user.lastName
-    const fullName: string | null = `${user.firstName} ${user.lastName}`
+    const firstName = user.firstName
+    const lastName = user.lastName
     const username = user.username
     return {
         id: user.id,
         username: username,
-        fullName: fullName,
+        fullName: `${firstName} ${lastName}`,
         profileImageUrl: user.profileImageUrl
     }
 }
